f3a9fa6e0b3f85f1f7d365447e3c75b78f0bec7b
Gustavo <gr4865423@gmail.com>
2026-03-24 09:42:34 -0300

    ativa IA no bot

--- server.js
+++ server.js
@@ -3,23 +3,65 @@ import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import twilio from "twilio";
 
+import { entenderMensagem } from "./openai.js";
+import { getUser, salvarReserva } from "./fluxos.js";
+
 dotenv.config();
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post("/webhook", (req, res) => {
+app.post("/webhook", async (req, res) => {
   const MessagingResponse = twilio.twiml.MessagingResponse;
   const twiml = new MessagingResponse();
 
   const msg = req.body.Body;
+  const numero = req.body.From;
 
-  console.log("Mensagem recebida:", msg);
+  console.log("Mensagem:", msg);
 
-  twiml.message("🔥 BOT ONLINE FUNCIONANDO");
+  const user = getUser(numero);
+  const ia = await entenderMensagem(msg);
 
-  res.type("text/xml");
-  res.send(twiml.toString());
+  if (ia.data) user.dados.data = ia.data;
+  if (ia.horario) user.dados.horario = ia.horario;
+  if (ia.pessoas) user.dados.pessoas = ia.pessoas;
+
+  if (user.dados.data && user.dados.horario && user.dados.pessoas) {
+    const reserva = salvarReserva(numero, user.dados);
+
+    user.dados = {};
+
+    twiml.message(`
+✅ Reserva confirmada!
+
+📅 ${reserva.data}
+⏰ ${reserva.horario}
+👥 ${reserva.pessoas} pessoas
+
+Aguardamos você 🍕🔥
+`);
+    
+    return res.type("text/xml").send(twiml.toString());
+  }
+
+  if (!user.dados.data) {
+    twiml.message("Qual dia deseja reservar? 📅");
+    return res.type("text/xml").send(twiml.toString());
+  }
+
+  if (!user.dados.horario) {
+    twiml.message("Qual horário? ⏰");
+    return res.type("text/xml").send(twiml.toString());
+  }
+
+  if (!user.dados.pessoas) {
+    twiml.message("Para quantas pessoas? 👥");
+    return res.type("text/xml").send(twiml.toString());
+  }
+
+  twiml.message("Não entendi, pode reformular?");
+  res.type("text/xml").send(twiml.toString());
 });
 
 app.listen(process.env.PORT || 3000, () => {
