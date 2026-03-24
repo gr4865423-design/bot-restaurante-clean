b8d3b07a53e65bf4e92b762c78f08f37935b0523
Gustavo <gr4865423@gmail.com>
2026-03-24 09:35:59 -0300

    fix twilio resposta

--- server.js
+++ server.js
@@ -10,13 +10,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post("/webhook", (req, res) => {
   const MessagingResponse = twilio.twiml.MessagingResponse;
-
   const twiml = new MessagingResponse();
+
+  const msg = req.body.Body;
+
+  console.log("Mensagem recebida:", msg);
+
   twiml.message("🔥 BOT ONLINE FUNCIONANDO");
 
-  res.type("text/xml").send(twiml.toString());
+  res.type("text/xml");
+  res.send(twiml.toString());
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Servidor rodando 🚀");
 });
